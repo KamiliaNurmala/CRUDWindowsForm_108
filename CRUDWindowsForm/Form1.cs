6e2c4e121ef89996ee8cefdb8e18d50e397b52d1
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 18:00:37 +0700

    Membuka blok using untuk objek SqlCommand dengan nama cmd, yang dibuat menggunakan query yang sudah didefinisikan dan koneksi conn. Objek ini akan dieksekusi untuk menambahkan data.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -76,6 +76,8 @@ namespace CRUDWindowsForm
                     conn.Open();
                     string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) " +
                        "VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn)) { }
                 }
             }
         }
