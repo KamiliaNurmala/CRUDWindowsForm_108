fb5bc407baed754fd81b6ca290999880849401c3
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 19:26:07 +0700

    Memeriksa apakah ada baris yang terpengaruh (lebih dari 0). Jika benar, berarti update data berhasil.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -202,6 +202,8 @@ namespace CRUDWindowsForm
                         cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
 
                         int rowAffected = cmd.ExecuteNonQuery();
+
+                        if (rowAffected > 0) { }
                     }
                 }
             }
