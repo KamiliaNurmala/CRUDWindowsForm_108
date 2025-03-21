0d22d30a9fd6cea7c07901ea11a0561ccd73b51e
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 18:29:39 +0700

    Menampilkan pesan error dengan detail exception (ex.Message), menggunakan judul 'Kesalahan' dan ikon error.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -156,7 +156,13 @@ namespace CRUDWindowsForm
                                 }
                             }
                         }
-                        catch (Exception ex) { }
+                        catch (Exception ex) 
+                        {
+                            MessageBox.Show("Error: " + ex.Message,
+                            "Kesalahan",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                        }
                     }
                 }
             }
