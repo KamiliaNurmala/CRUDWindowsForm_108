4e7e7e7b1bc05df51373b256040a279a2675681f
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 18:07:14 +0700

    Memanggil metode ClearForm() untuk mengosongkan kontrol input sehingga siap untuk entri data baru.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -95,6 +95,7 @@ namespace CRUDWindowsForm
                             LoadData();
                             ClearForm();
                         }
+                        else { }
                     }
                 }
             }
