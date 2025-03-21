2e7fe6de6c2d46ea1733b2f86b10fa126da18058
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 18:22:52 +0700

    Jika rowAffected tidak lebih dari 0 (artinya tidak ada data yang dihapus), masuk ke blok else.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -147,6 +147,7 @@ namespace CRUDWindowsForm
                                     LoadData();
                                     ClearForm();
                                 }
+                                else { }
                             }
                         }
                     }
