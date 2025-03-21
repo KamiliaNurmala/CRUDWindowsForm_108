290cdaef1391abbcd520e49b8269ff13e8617200
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 19:03:57 +0700

    Mengatur nilai teks dari kontrol txtAlamat dengan nilai dari sel kelima (indeks 4) pada baris yang diklik, dengan perlakuan null-conditional.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -191,6 +191,7 @@ namespace CRUDWindowsForm
                 txtNama.Text = row.Cells[1].Value?.ToString();
                 txtEmail.Text = row.Cells[2].Value?.ToString();
                 txtTelepon.Text = row.Cells[3].Value?.ToString();
+                txtAlamat.Text = row.Cells[4].Value?.ToString();
             }
 
         }
