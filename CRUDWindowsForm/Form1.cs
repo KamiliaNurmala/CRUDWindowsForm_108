c7c44a72ff5352eb9902237961410efdd3f5f322
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 19:25:16 +0700

    Menambahkan parameter @Alamat dengan nilai dari kontrol txtAlamat.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -199,6 +199,7 @@ namespace CRUDWindowsForm
                         cmd.Parameters.AddWithValue("@Nama", txtNama.Text);
                         cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
                         cmd.Parameters.AddWithValue("@Telepon", txtTelepon.Text);
+                        cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
                     }
                 }
             }
