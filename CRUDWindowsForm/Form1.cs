75437fdd4459b4b807b9b9dd52975e66dc41f293
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 17:29:20 +0700

    Memanggil metode LoadData() untuk mengambil dan menampilkan data dari database ketika form dimuat.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -14,7 +14,10 @@ namespace CRUDWindowsForm
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+            LoadData();
+        }
     }
 
 }
