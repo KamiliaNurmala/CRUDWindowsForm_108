1e633eb848b1f8f1a24cccc30090d641bdd5b6ef
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-03-21 19:22:54 +0700

    Membuka blok using untuk membuat objek SqlCommand bernama cmd yang digunakan untuk mengeksekusi query dengan koneksi conn.

--- CRUDWindowsForm/Form1.cs
+++ CRUDWindowsForm/Form1.cs
@@ -193,6 +193,7 @@ namespace CRUDWindowsForm
                     conn.Open();
                     string query = "UPDATE Mahasiswa SET Nama = @Nama, Email = @Email, " +
                        "Telepon = @Telepon, Alamat = @Alamat WHERE NIM = @NIM";
+                    using (SqlCommand cmd = new SqlCommand(query, conn)) { }
                 }
             }
 
